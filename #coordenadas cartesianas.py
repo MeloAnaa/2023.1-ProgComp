4ca6e29e1f26c23adaf6def19ad92aa92d11ef12
ana clara melo de araujo <118742655+MeloAnaa@users.noreply.github.com>
2023-04-10 21:28:34 -0300

    coordenadas cartesianas

---
+++ #coordenadas cartesianas.py
@@ -0,0 +1,18 @@
+#coordenadas cartesianas
+coordenada1= int(input("digite primeira coordenada"))
+coordenada2= int(input("digite segunda coordenada"))
+if coordenada1 >=0 and coordenada2 >=0:
+    print("sua coordenada encontra-se no primeiro quadrante")
+else:
+       if coordenada1 >=0 and coordenada2 <0:
+             print("sua coordenada encontra-se no quarto quadrante")
+       else:
+             if coordenada1 <0 and coordenada2 >=0:
+                   print("sua coordenada encontra-se no segundo quadrante")
+             else:
+                   if coordenada <0 and coordenada2 <0:
+                         print("sua coordenada encontra-se no terceiro quadrante")
+                   
+
+    
+ 
