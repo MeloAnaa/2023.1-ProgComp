2b53e188eed1df1294d380596bc4b56478f2bae2
ana clara melo de araujo <118742655+MeloAnaa@users.noreply.github.com>
2023-05-02 17:54:13 -0300

    Add files via upload
    resolver

---
+++ '''programa com laço de repeticao.py
@@ -0,0 +1,34 @@
+'''fzr um programa que solicite 2 valores inteiros positivos e imprima a soma de todos os 
+inteiros entre esses valores, incluindo eles.
+y>x'''
+
+x=int(input("informe o primeiro valor:"))
+y=int(input("informe o segundo valor:"))
+n= y-x+1
+soma= x+y *n /2
+
+if y>x:
+   print(f"{mult} + {x} = {x}+{y}")
+print(f"{mult} + {y} = {x}+{y}")
+   
+    
+
+'''
+valor= int(input("informe um valor inteiro positivo:"))
+mult=1
+
+if (valor >0):
+    print(f"{mult} x {valor} = {mult}*{valor}")
+    mult += 1 '''
+
+
+
+
+'''valor= int(input("informe um valor inteiro positivo:"))
+if (valor >0):
+    while (mult <=10):
+        print(f"{mult} x {valor} = {mult*valor}")
+        mult +=1
+    else:
+        print("valor invalido") '''
+
